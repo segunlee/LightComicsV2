498ed4176b403ceec50075946364037bd5ae2825
LEE SEGUN <segunleedev@gmail.com>
2021-12-20 16:23:14 +0900

    Initial Commit

---
+++ LightComicsV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightComicsV2
+//
+//  Created by LeeSeGun on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
